2501787684da36cf432a2221d08565539d7d6cb2
cleanaz-dev <founder@cleanazcoding.com>
2025-01-16 20:09:55 -0500

    edit reading log

--- app/(dashboard)/chat/page.jsx
+++ app/(dashboard)/chat/page.jsx
@@ -1,6 +1,6 @@
 import ChatBot from '@/components/chat-bot/ChatBot';
 
-export default function Home() {
+export default function page() {
   return (
     <div className="flex container mx-auto">
       <ChatBot />
