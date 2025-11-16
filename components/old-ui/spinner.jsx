6479d96bfe5e62f96d667d4888e26c2bf6a3e43b
cleanaz-dev <founder@cleanazcoding.com>
2025-11-16 15:35:15 -0500

    updated conversation ai to use moonshot and aws polly

--- components/old-ui/spinner.jsx
+++
@@ -1,6 +0,0 @@
-// components/ui/spinner.jsx
-export function Spinner() {
-  return (
-    <div className="h-5 w-5 animate-spin rounded-full border-2 border-primary border-t-transparent" />
-  );
-}
