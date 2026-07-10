80b2d1f9fa08c3b54f6245425256c07c5b7a3715
CleanAZCoding <founder@cleanazcoding.com>
2026-07-09 21:09:25 -0400

    updates

--- proxy.js
+++ proxy.js
@@ -2,14 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
 
-// Renamed to 'proxy' - that's the ONLY change needed
-export function proxy(auth, req) {
-  return clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) {
-      await auth.protect();
-    }
-  })(auth, req);
-}
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
