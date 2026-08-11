520535e46b700c66a69eca8f6f0e0ee19d00d8a3
CleanAZCoding <founder@cleanazcoding.com>
2026-08-11 14:08:47 -0400

    updates

--- app/api/audio/route.ts
+++ app/api/audio/route.ts
@@ -2,8 +2,8 @@ import { useSearchParams } from "next/navigation";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // get 'key' from search params
-  const audioKey = useSearchParams();
+//   // get 'key' from search params
+//   const audioKey = useSearchParams();
 
   return NextResponse.json({ message: "success" });
 }
