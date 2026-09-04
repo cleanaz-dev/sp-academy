08605137d7444e2f1ad247be00f4d95b5d8eb29c
CleanAZCoding <founder@cleanazcoding.com>
2026-09-04 12:00:29 -0400

    up

--- app/(dashboard)/learning-hub/page.tsx
+++ app/(dashboard)/learning-hub/page.tsx
@@ -1,10 +1,19 @@
 import LearningHubPage from "@/components/learning-hub/LearningHubPage";
-import { getAllUserReviews, getUserbyUserId, type UserReviews } from "@/lib/actions";
+import {
+  getAllUserReviews,
+  getUserbyUserId,
+  type UserReviews,
+} from "@/lib/actions";
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const { userId: clerkId } = await auth();
   const user = await getUserbyUserId(clerkId);
+
+  if (!user) {
+    redirect("/sign-in");
+  }
   const reviews: UserReviews = await getAllUserReviews(user.id);
 
   console.log("reviews", reviews);
