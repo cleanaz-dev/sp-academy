b779fad0fb0896a2fe30e34baf45c7d6c591458d
CleanAZCoding <founder@cleanazcoding.com>
2026-09-18 14:15:57 -0400

    up

--- app/(dashboard)/foundation/page.tsx
+++ app/(dashboard)/foundation/page.tsx
@@ -2,7 +2,7 @@ import { FoundationWrapper } from "@/components/foundation/foundation-wrapper";
 
 export default async function FoundationPage() {
     return (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen animate-bg">
             <FoundationWrapper />
         </div>
     );
